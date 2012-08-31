3096c52e456cf836660eb8b8c351036dd229ee70
John Crepezzi <john.crepezzi@gmail.com>
2012-08-31 09:11:37 -0400

    Hey

---
+++ example.js
@@ -0,0 +1,38 @@
+var Session = require('./lib/jsprof/session');
+
+
+var Thing = function () { };
+
+Thing.doIt = function () {
+  var t = new Thing();
+  t.doThings();
+  t.doThing();
+};
+
+Thing.prototype.doThings = function () {
+  for (var i = 0; i < 10; i++) {
+    this.doThing();
+    this.doOtherThing();
+    this.doOtherThing();
+  }
+};
+
+Thing.prototype.doOtherThing = function () {
+};
+
+Thing.prototype.doThing = function () {
+};
+
+//////
+
+var session = new Session({
+  'Thing': Thing,
+  'Thing.prototype': Thing.prototype
+});
+session.profile(function () {
+  Thing.doIt();
+});
+
+session.printCallTimes();
+console.log();
+session.printDrillTimes();
